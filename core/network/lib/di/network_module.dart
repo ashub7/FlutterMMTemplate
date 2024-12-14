1ba5bac35686515ca1731bddca8d551d99dcc2d7
ashutosh <ashutosh.bhandari@goldenscent.com>
2024-12-14 14:51:58 +0530

    Comment added

--- core/network/lib/di/network_module.dart
+++ core/network/lib/di/network_module.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-
+// Network DI module
 @module
 abstract class NetworkModule {
 
